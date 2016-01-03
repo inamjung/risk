147cda31abb811b0f8769a7aef08d5b3a6c9a723
swl <swl@inam>
2016-01-03 22:01:56 +0700

    เพิ่ม/แก้ไข

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=risks',
+    'username' => 'sa',
+    'password' => 'sa',
     'charset' => 'utf8',
 ];
